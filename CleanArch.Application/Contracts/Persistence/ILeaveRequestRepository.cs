57f90f77e03350afa8dcc3550c29897f99dac8ec
intelejentboy <32359711+intelejentboy@users.noreply.github.com>
2025-02-27 23:18:23 +0330

    complete LeaveRequestRepository

--- CleanArch.Application/Contracts/Persistence/ILeaveRequestRepository.cs
+++ CleanArch.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -1,10 +1,14 @@
-﻿
+
 using CleanArch.Domain;
 
 namespace CleanArch.Application.Contracts.Persistence;
 
 public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
 {
+    Task<LeaveRequest> GetLeaveRequestWithDetails(int id);
 
+    Task<List<LeaveRequest>> GetLeaveRequestWithDetails();
+
+    Task<List<LeaveRequest>> GetLeaveRequestWithDetails(string userId);
 }
 
--- CleanArch.Persistence/Repositories/LeaveRequestRepository.cs
+++ CleanArch.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,7 +1,8 @@
-﻿
+
 using CleanArch.Application.Contracts.Persistence;
 using CleanArch.Domain;
 using CleanArch.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArch.Persistence.Repositories;
 
@@ -9,5 +10,33 @@ public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveReq
 {
     public LeaveRequestRepository(CADatabaseContext context) : base(context)
     {
+
     }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestWithDetails()
+    {
+        var leaveReuests = await _context.LeaveRequests
+          .Include(q => q.LeaveType)
+          .ToListAsync();
+
+        return leaveReuests;
+    }
+    public async Task<List<LeaveRequest>> GetLeaveRequestWithDetails(string userId)
+    {
+        var leaveReuests = await _context.LeaveRequests
+             .Where(q => q.RequestingEmployeeId == userId)
+             .Include(q => q.LeaveType)
+             .ToListAsync();
+
+        return leaveReuests;
+    }
+    public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+    {
+        var leaveReuest = await _context.LeaveRequests
+            .Include(q => q.LeaveType).FirstOrDefaultAsync(q => q.Id == id);
+        return leaveReuest;
+    }
+}
+
+   
 }
