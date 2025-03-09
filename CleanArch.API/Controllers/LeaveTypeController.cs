29768cfbc98c07f4651daca9115991f7a3af863e
intelejentboy <32359711+intelejentboy@users.noreply.github.com>
2025-03-09 18:59:26 +0330

    add other apis for leavetype

--- CleanArch.API/Controllers/LeaveTypeController.cs
+++ CleanArch.API/Controllers/LeaveTypeController.cs
@@ -1,6 +1,12 @@
+using CleanArch.Application.Features.LeaveType.Commands.CreateLeaveType;
+using CleanArch.Application.Features.LeaveType.Commands.DeleteLeaveType;
+using CleanArch.Application.Features.LeaveType.Commands.UpdateLeaveType;
 using CleanArch.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using CleanArch.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using CleanArch.Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -27,27 +33,43 @@ namespace CleanArch.API.Controllers
 
         // GET api/<LeaveTypeController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<LeaveTypeDetailsDTO>> Get(int id)
         {
-            return "value";
+            var leaveType = await _mediator.Send(new GetLeaveTypeDetailsQuery(id));
+            return Ok(leaveType);
         }
 
         // POST api/<LeaveTypeController>
         [HttpPost]
-        public void Post([FromBody]string value)
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult> Post(CreateLeaveTypeCommand leaveType)
         {
+            var response = await _mediator.Send(leaveType);
+            return CreatedAtAction(nameof(Get) , new { id = response });
         }
 
         // PUT api/<LeaveTypeController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Put(UpdateLeaveTypeCommand leaveType)
         {
+           await _mediator.Send(leaveType);
+            return NoContent();
         }
 
         // DELETE api/<LeaveTypeController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(int id)
         {
+            DeleteLeaveTypeCommand command = new DeleteLeaveTypeCommand() { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
         }
     }
 }
